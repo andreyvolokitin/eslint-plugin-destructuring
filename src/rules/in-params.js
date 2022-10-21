402c0b5b652a71112fc4c81a59644d91466c90b4
andreyvolokitin <created42@gmail.com>
2022-10-22 02:05:53 +0400

    Allow linting for a maximum number of destructured properties (#1)
    * Allow linting for a maximum number of destructured properties
    
    * Lint params of function expressions
    
    * Define default of 3 for max-destructured, add tests
    
    * Scope package, bump version
    
    * improvements: fix: use fork URL in `package.json`
    
    * Bump version

--- src/rules/in-params.js
+++ src/rules/in-params.js
@@ -14,6 +14,10 @@ module.exports = {
             type: 'integer',
             minimum: 0,
           },
+          'max-destructured': {
+            type: 'integer',
+            minimum: 0,
+          },
         },
         additionalProperties: false,
       },
@@ -23,20 +27,31 @@ module.exports = {
   create: function inParams(context) {
     const option = context.options[0];
     let maxParams = 1;
+    let maxDestructured = 3;
 
-    if (typeof option === 'object' &&
-      Object.prototype.hasOwnProperty.call(option, 'max-params') &&
-      typeof option['max-params'] === 'number') {
-      maxParams = option['max-params'];
+    if (typeof option === 'object') {
+      if (Object.prototype.hasOwnProperty.call(option, 'max-params') && typeof option['max-params'] === 'number') {
+        maxParams = option['max-params'];
+      }
+
+      if (Object.prototype.hasOwnProperty.call(option, 'max-destructured') && typeof option['max-destructured'] === 'number') {
+        maxDestructured = option['max-destructured'];
+      }
     }
+
 
     return {
       ObjectPattern(node) {
         if (node.parent.type === 'ArrowFunctionExpression' ||
-        node.parent.type === 'FunctionDeclaration') {
+        node.parent.type === 'FunctionDeclaration' ||
+          node.parent.type === 'FunctionExpression') {
           if (node.parent.params.length > maxParams) {
             context.report(node, 'Do not use destructuring in params when there' +
               ` are more than ${maxParams} params.`);
+          }
+
+          if (node.properties.length > maxDestructured) {
+            context.report(node, `Do not destructure more than ${maxDestructured} properties when inside params`);
           }
         }
       },
